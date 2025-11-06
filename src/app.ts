48d893bcf7a22e838f95b89c5aadea6acd5ffd88
GABRIEL THEMER BRITO <gabriel.tbrito@senacsp.edu.br>
2025-11-06 15:24:47 -0300

    Feat|Test: escrito exemplo de acesso ao conteudo. #2

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,31 @@ import express from "express";
 import { Request, Response, NextFunction } from "express";
 
 const app = express();
+app.use(express.json())
+
+
+app.get("/parametro/:nome",(req:Request,res:Response,next:NextFunction)=>{
+    const nome = req.params.nome
+    console.log("Rota de parametro - Cliente digitou:",nome)
+    res.send(`voce digitou o nome ${nome}`)
+})
+
+app.get("/query",(req:Request,res:Response,next:NextFunction)=>{
+    const nome = req.query.nome
+    console.log("Cliente digitou:",nome)
+    res.send(`voce digitou o nome ${nome}`)
+})
+
+app.get("/corpo",(req:Request,res:Response,next:NextFunction)=>{
+    const nome = req.body.nome;
+    console.log("Variavel dentro do corpo - Cliente digitou:",nome)
+    res.send(`voce digitou o nome ${nome}`)
+})
+
+app.get("/exemplo",(req:Request,res:Response,next:NextFunction)=>{
+  console.log('aconteceu algo')
+  res.send("rodou tudo certo!!")
+})
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.send("Hello word!!");
