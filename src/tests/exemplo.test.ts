afc10cd88802bcdfb10589aba4625e755eda0ff8
GABRIEL THEMER BRITO <gabriel.tbrito@senacsp.edu.br>
2025-12-03 14:33:16 -0300

    exemplo dos testes das tarefas

---
+++ src/tests/exemplo.test.ts
@@ -0,0 +1,9 @@
+  test("exemplo", ()=>{
+    const nome = "teste da silva"
+    expect(nome).toBe("teste da silva sauro")
+  })
+
+    test("exemplo2", ()=>{
+    const result = 5+5;
+    expect(result).toBe(10)
+  })
