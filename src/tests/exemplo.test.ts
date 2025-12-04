649e132fee1575de3547a8599f3e1f1f8795e44d
GABRIEL THEMER BRITO <gabriel.tbrito@senacsp.edu.br>
2025-12-04 16:19:06 -0300

    corrigido os teste da tarefa

--- src/tests/exemplo.test.ts
+++ src/tests/exemplo.test.ts
@@ -1,10 +1,72 @@
   
-  test("GET: /tarefas = 200", async ()=>{
-    const res = await fetch("http://localhost:3000/task/")
-    expect(res.status).toBe(200)
+const URL_base = "http://localhost:3000/task"
 
-    const body = await res.json()
-    expect(Array.isArray(body)).toBe(true) 
+let tarefa_id: number = 0
+
+const nova_tarefa ={
+  nome: "Nome da tarefa",
+  descricao: "atualizou"
+}
+
+const tarefa_atualizada ={
+  nome: "Nome da tarefa atualizada",
+  descricao: "Descricao da tarefa de exemplo",
+};
+
+test("GET: /tarefas = 200", async ()=>{
+  const res = await fetch(URL_base)
+  expect(res.status).toBe(200)
+
+  const body = await res.json()
+  expect(Array.isArray(body)).toBe(true) 
+})
+
+test("POST: /task = 201(Criar tarefa",async()=>{
+  const res = await fetch(URL_base,{
+    method: "POST",
+    headers:{"Content-Type": "application/json"},
+    body: JSON.stringify(nova_tarefa)
+  });
+  expect(res.status).toBe(201);
+
+  const content = await res.json()
+  tarefa_id = content.id
+
+  expect(content).toHaveProperty("id")
+  expect(content).toHaveProperty("nome")
+  expect(content).toHaveProperty("descricao")
+})
+
+test("GET: /task = 200", async()=>{
+  const res = await fetch(`${URL_base}/${tarefa_id}`)
+  expect(res.status).toBe(200);
+
+  const content = await res.json()
+  expect(content).toHaveProperty("nome",nova_tarefa["nome"])
+  expect(content).toHaveProperty("descricao",nova_tarefa["descricao"])
+})
+
+test ("PUT: /task/1 = 200 (atualizar tarefa)", async()=>{
+  const res = await fetch(`${URL_base}/${tarefa_id}`,{
+    method:"PUT",
+    headers:{"Content-Type": "application/json"},
+    body: JSON.stringify(tarefa_atualizada)
   })
+  expect(res.status).toBe(201);
+})
+
+test("DELETE: /task/:id = 204",async()=>{
+  const res = await fetch(`${URL_base}/${tarefa_id}`,{
+    method:"DELETE"
+  })
+  expect(res.status).toBe(200)
+
+  const content = await res.json()
+
+  expect(content).toHaveProperty("id")
+  expect(content).toHaveProperty("nome")
+  expect(content).toHaveProperty("descricao")
+
+})
 
 
